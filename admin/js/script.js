29d01c577d43755eb1d4137fe8d637cf003cef89
DevIdol <devidol.mm@gmail.com>
2022-02-24 19:06:59 +0630

    add user list

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $('#menu-btn').click(function(){
-        $('#menu').toggleClass('actived');
-    })
+$(document).ready(function () {
+  $('#menu .items li, #menu .items li i, #menu .items li a').on('click', function () {
+    $('#menu .items li, #menu .items li i, #menu .items li a').removeClass('active')
+    $(this).addClass('active')
+  })
+
+  $('#menu-btn').click(function () {
+    $('#menu').toggleClass('actived')
+  })
 })
